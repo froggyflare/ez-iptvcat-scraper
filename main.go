147e1914158a4f42003c544cef6ec31290712572
froggyflare <115334120+froggyflare@users.noreply.github.com>
2022-10-08 17:48:42 -0400

    fix

--- main.go
+++ main.go
@@ -172,7 +172,7 @@ func writeToFile() {
 		ioutil.WriteFile("data/countries/"+key+".json", streamsCountry, 0644)
 	}
 
-	f, err := os.Open("data/all-streams.m3du", os.O_CREATE, 0644)
+	f, err := os.Open("data/all-streams.m3du", os.O_CREATE)
 	if err != nil {
 		fmt.Println("error opening m3du file:", err)
 	}
